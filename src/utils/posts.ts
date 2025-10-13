d94cd6aa991688e3a37e93bb1373fe8540092373
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-10-13 12:17:16 -0300

    animate blog post index->post

---
+++ src/utils/posts.ts
@@ -0,0 +1,23 @@
+type Post = {
+  url: string;
+  frontmatter: {
+    title: string;
+    author: string;
+    date: string; // yyyy-mm-dd
+    tags: Array<string>;
+  };
+};
+
+export const getPosts = () =>
+  Object.values(
+    import.meta.glob<Post>("../pages/blogs/*.md", { eager: true }),
+  ).sort(
+    (a, b) =>
+      new Date(b.frontmatter.date).getTime() -
+      new Date(a.frontmatter.date).getTime(),
+  );
+
+export const getPostIndex = (title: string) => {
+  let posts = getPosts();
+  return posts.findIndex((post) => post.frontmatter.title === title);
+};
